29c97982b83a22849c38fe2209094f44bc2de207
ridaDev <kiratterida@gmail.com>
2024-12-18 10:21:35 +0100

    créé classe Personne et constrecteur

---
+++ Personne.java
@@ -0,0 +1,14 @@
+abstract class Personne {
+    int id ;
+    String nom;
+    String prenom;
+    String email;
+
+    public Personne( String nom, String prenom, String email) {
+        this.id = 1;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+    }
+}
+
