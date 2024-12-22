68e49f3e7ced2b418d9744deb0120e1f749eabb7
ridaDev <kiratterida@gmail.com>
2024-12-22 14:39:10 +0100

    Ajouter fonction AjouterFormateur

--- Formateur.java
+++ Formateur.java
@@ -36,6 +36,23 @@ public Formateur( int id , String nom, String prenom, String email, String speci
                 ", salaire=" + salaire +
                 '}';
     }
+    static void AjouterFormateur(){
+        id = formateurs.size()+1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter nom de Formateur : ");
+        String nom = sc.nextLine();
+        System.out.println("Enter prenom de Formateur : ");
+        String prenom= sc.nextLine();
+        System.out.println("Enter email(@gmail.com) de Formateur : ");
+        String email = sc.nextLine();
+        System.out.println("Enter specialite de Formateur : ");
+        String specialite = sc.nextLine();
+        System.out.println("Enter salaire de Formateur : ");
+        double salaire = sc.nextDouble();
 
+        Formateur form = new Formateur(id, nom, prenom,email,specialite,salaire);
+        formateurs.add(form);
+        System.out.println();
+    }
 
 }
